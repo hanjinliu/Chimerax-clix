de037294e1b409ef35aa2b25e6ecbdfdf603ecce
Hanjin Liu <liuhanjin-sc@i.softbank.jp>
2024-05-21 21:24:57 +0900

    better model comp wip

--- src/completion/_model.py
+++ src/completion/_model.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Iterator
 from .state import CompletionState
 
 ALL_ATOMS = ["Ca", "Cb", "C", "N", "O", "OH"]
@@ -35,9 +36,10 @@ def complete_model(session, last_word: str, current_command: str | None):
     # "#" -> "#1 (model name)" etc.
     # try model+chain specifiction completion such as "#1/B"
     if "/" in last_word:
-        model_spec, chain_spec = last_word.split("/", 1)
-        for model in session.models.list():
-            if _model_to_spec(model) == model_spec and hasattr(model, "chains"):
+        model_spec_str, chain_spec = last_word.split("/", 1)
+        model_spec = ModelSpec(model_spec_str)
+        for model in model_spec.filter(session.models.list()):
+            if hasattr(model, "chains"):
                 with_chain_ids: list[str] = list(
                     f"{model_spec}/{_i}" for _i in model.chains.chain_ids
                     if _i.startswith(chain_spec)
@@ -47,20 +49,21 @@ def complete_model(session, last_word: str, current_command: str | None):
                     ["<i>chain ID</i>"] * len(with_chain_ids), type="model,chain"
                 )
     if ":" in last_word:
-        model_spec, chain_spec = last_word.split(":", 1)
-        for model in session.models.list():
-            if _model_to_spec(model) == model_spec and hasattr(model, "nonstandard_residue_names"):
-                with_residues: list[str] = list(
-                    f"{model_spec}:{_r}" for _r in model.nonstandard_residue_names
-                    if _r.startswith(chain_spec)
-                )
-                return CompletionState(
-                    last_word, with_residues, current_command, 
-                    ["<i>residue</i>"] * len(with_residues), type="model,residue"
-                )
+        model_spec_str, chain_spec = last_word.split(":", 1)
+        model_spec = ModelSpec(model_spec_str)
+        for model in model_spec.filter(session.models.list()):
+            residue_names: list[str] = getattr(model, "nonstandard_residue_names", [])
+            with_residues: list[str] = list(
+                f"{model_spec}:{_r}" for _r in residue_names + ALL_AMINO_ACIDS
+                if _r.startswith(chain_spec)
+            )
+            return CompletionState(
+                last_word, with_residues, current_command, 
+                ["<i>residue</i>"] * len(with_residues), type="model,residue"
+            )
     if "@" in last_word:
-        _, atom_spec = last_word.split("@", 1)
-        all_atoms = [f"@{_a}" for _a in ALL_ATOMS if _a.startswith(atom_spec)]
+        model_spec_str, atom_spec = last_word.split("@", 1)
+        all_atoms = [f"{model_spec_str}@{_a}" for _a in ALL_ATOMS if _a.startswith(atom_spec)]
         return CompletionState(
             last_word, all_atoms, current_command, 
             ["<i>atom</i>"] * len(all_atoms), type="model,atom"
@@ -69,12 +72,36 @@ def complete_model(session, last_word: str, current_command: str | None):
     info = []
     for model in session.models.list():
         spec = _model_to_spec(model)
-        if spec.startswith(last_word):
-            comps.append(_model_to_spec(model))
+        if "," in last_word and "-" not in last_word:
+            former, num = last_word.rsplit(",", 1)
+            if f"{num}".startswith(spec[1:]):
+                comps.append(f"{former},{spec[1:]}")
+                info.append(model.name)
+        elif "-" in last_word:
+            former, num = last_word.rsplit("-", 1)
+            if num.startswith(spec[1:]):
+                comps.append(f"{former}-{spec[1:]}")
+                info.append(model.name)
+        elif spec.startswith(last_word):
+            comps.append(spec)
             info.append(model.name)
     return CompletionState(last_word, comps, current_command, info, type="model")
 
 def complete_chain(session, last_word: str, current_command: str | None):
+    if ":" in last_word:
+        chain_spec_str, residue_spec = last_word.split(":", 1)
+        chain_spec = ChainSpec(chain_spec_str)
+        for model in session.models.list():
+            if hasattr(model, "chains"):
+                chains = chain_spec.filter(model.chains)
+                with_residues: list[str] = list(
+                    f"{chain_spec_str}:{_c.chain_id}" for _c in chains
+                    if _c.chain_id.startswith(residue_spec)
+                )
+                return CompletionState(
+                    last_word, with_residues, current_command, 
+                    ["<i>residue</i>"] * len(with_residues), type="chain,residue"
+                )
     # collect all the available chain IDs
     all_chain_ids: set[str] = set()
     for model in session.models.list():
@@ -118,3 +145,60 @@ def complete_atom(session, last_word: str, current_command: str | None):
 
 def _model_to_spec(model):
     return "#" + ".".join(str(_id) for _id in model.id)
+
+
+class ModelSpec:
+    def __init__(self, spec: str):
+        ids: set[tuple[int, ...]] = set()
+        for s in spec.split(","):
+            if "." in s:
+                s0, s1 = s.split(".", 1)
+                if "-" in s1:
+                    # "#1.1-3" -> (1, 1), (1, 2), (1, 3)
+                    try:
+                        model_id = int(s0)
+                    except ValueError:
+                        continue
+                    start, end = s1.split("-", 1)
+                    ids.update((model_id, x) for x in _safe_range(start, end))
+                elif "-" in s0:
+                    # not understood
+                    continue
+                else:
+                    ids.add((int(s0), int(s1)))
+            else:
+                if "-" in s:
+                    start, end = s.split("-", 1)
+                    ids.update((x,) for x in _safe_range(start, end))
+                else:
+                    ids.add((int(s),))
+        self.ids = ids
+
+    def filter(self, models: list) -> list:
+        return [m for m in models if m.id in self.ids]
+
+class ChainSpec:
+    def __init__(self, spec: str):
+        ids: set[int] = set()
+        for s in spec.split(","):
+            if "-" in s:
+                start, end = s.split("-", 1)
+                ids.update(x for x in _safe_char_range(start, end))
+            else:
+                ids.add(s)
+        self.ids = ids
+    
+    def filter(self, chains: list) -> list:
+        return [c for c in chains if ord(c.chain_id) in self.ids]
+
+def _safe_range(start: str, end: str) -> range:
+    try:
+        return range(int(start), int(end) + 1)
+    except ValueError:
+        return range(0)
+
+def _safe_char_range(start: str, end: str) -> Iterator[int]:
+    try:
+        return iter(chr(i) for i in range(ord(start), ord(end) + 1))
+    except (TypeError, ValueError):
+        return iter([])
