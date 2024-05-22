3134a44f2109627f1ec2dc67ded670dbb4ae9a58
Hanjin Liu <liuhanjin-sc@i.softbank.jp>
2024-05-22 14:12:53 +0900

    add tooltip

--- src/widgets/preference_dialog.py
+++ src/widgets/preference_dialog.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from .._preference import Preference, load_preference, save_preference
 
-from qtpy import QtWidgets as QtW, QtGui, QtCore
+from qtpy import QtWidgets as QtW
 
 class QShowDialogButton(QtW.QPushButton):
     def __init__(self, parent: QtW.QWidget | None = None):
@@ -30,20 +30,42 @@ class QPreferenceDialog(QtW.QDialog):
 
         self._area = QtW.QComboBox()
         self._area.addItems(["top", "side", "bottom"])
+        self._area.setToolTip("The area where the CliX widget will be placed")
         self._area.setCurrentText(preference.area)
         layout.addRow("CliX widget area", self._area)
 
         self._hide_title_bar = QtW.QCheckBox("Hide title bar")
+        self._hide_title_bar.setToolTip(
+            "This will hide the title bar of the CliX widget, just like the built-in "
+            "command line."
+        )
         self._hide_title_bar.setChecked(preference.hide_title_bar)
         layout.addRow(self._hide_title_bar)
 
         self._show_label = QtW.QCheckBox("Show \"CliX\" label on the left")
+        self._show_label.setToolTip(
+            "This will show the \"CliX\" label on the left of the CliX widget command "
+            "line."
+        )
         self._show_label.setChecked(preference.show_label)
         layout.addRow(self._show_label)
         
         self._enter_completion = QtW.QCheckBox("Enable Enter key for completion")
+        self._enter_completion.setToolTip(
+            "Pressing Enter key will select the completion suggestion, instead of "
+            "executing the command."
+        )
         self._enter_completion.setChecked(preference.enter_completion)
         layout.addRow(self._enter_completion)
+
+        self._auto_focus = QtW.QCheckBox("Focus on CliX widget when typing")
+        self._auto_focus.setToolTip(
+            "Automatically focus on the CliX widget when typing. This option is useful "
+            "when you have to go back and forth between the CliX widget and the main "
+            "window."
+        )
+        self._auto_focus.setChecked(preference.auto_focus)
+        layout.addRow(self._auto_focus)
 
         buttons = QtW.QDialogButtonBox()
         buttons.setStandardButtons(
@@ -60,5 +82,6 @@ class QPreferenceDialog(QtW.QDialog):
             hide_title_bar=self._hide_title_bar.isChecked(),
             show_label=self._show_label.isChecked(),
             enter_completion=self._enter_completion.isChecked(),
+            auto_focus=self._auto_focus.isChecked(),
         )
         self.accept()
