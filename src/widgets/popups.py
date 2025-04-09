97aa0df634b421f734938b5fa365f4db71c63c7e
Hanjin Liu <liuhanjin-sc@i.softbank.jp>
2025-04-10 08:58:03 +0900

    fix py<3.9 annotation and add test

--- src/widgets/popups.py
+++ src/widgets/popups.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from functools import cache
-import math
 from pathlib import Path
 from typing import Iterator
 from qtpy import QtWidgets as QtW, QtGui, QtCore
